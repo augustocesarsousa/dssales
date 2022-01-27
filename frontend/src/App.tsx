9007e09fee5ab866d0d5e73f3b120d257785126d
Augusto Cesar <acs-03@hotmail.com>
2022-01-26 21:49:19 -0300

    Component Filter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Filter from './components/filter';
 import Header from './components/header';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="app-container">
+        <Filter />
+      </div>
     </>
   );
 }

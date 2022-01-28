e7960cdda953cb9fde63fff5859400d205152ee4
Augusto Cesar <acs-03@hotmail.com>
2022-01-27 21:29:14 -0300

    Component SalesBySummary - part 2

--- frontend/src/components/sales-summary/sales-summary-card.tsx
+++ frontend/src/components/sales-summary/sales-summary-card.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { ReactComponent as AvatarIcon } from '../../assets/images/avatar-icon.svg';
+import './styles.css';
 
 function SalesSummaryCard() {
-  return <div className="sales-summary-card base-card"></div>;
+  return (
+    <div className="sales-summary-card base-card">
+      <AvatarIcon />
+      <h3 className="sales-summar-card-value">534.00</h3>
+      <span className="sales-summar-card-label">Média</span>
+    </div>
+  );
 }
 
 export default SalesSummaryCard;

633a322f8d79896c085f0d5896857ee8f5781e20
Augusto Cesar <acs-03@hotmail.com>
2022-01-27 22:22:06 -0300

    Component PieChartCard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Filter from './components/filter';
 import Header from './components/header';
+import PieChartCard from './components/pie-chart-card';
 import SalesByDate from './components/sales-by-date';
 import SalesSummary from './components/sales-summary';
 
@@ -14,6 +15,16 @@ function App() {
         <SalesByDate />
         <div className="sales-overview-container">
           <SalesSummary />
+          <PieChartCard
+            name="Lojas"
+            labels={['Francisco Morato', 'Franco da Rocha', 'Várzea Paulista']}
+            series={[50, 30, 20]}
+          />
+          <PieChartCard
+            name="Pagamento"
+            labels={['Crédito', 'Débito', 'Dinheiro']}
+            series={[40, 35, 25]}
+          />
         </div>
       </div>
     </>

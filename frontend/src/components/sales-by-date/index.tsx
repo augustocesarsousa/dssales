f9186ce7448216e7e00b74004f36fea0441345d0
Augusto Cesar <acs-03@hotmail.com>
2022-01-28 22:33:51 -0300

    Backend integration SalesByDate - part 2

--- frontend/src/components/sales-by-date/index.tsx
+++ frontend/src/components/sales-by-date/index.tsx
@@ -1,52 +1,19 @@
 import { useEffect, useState } from 'react';
 import ReactApexChart from 'react-apexcharts';
+import { ChartSeriesData, SalesByDate } from '../../types';
 import { makeRequest } from '../../utils/request';
-import { chartOptions } from './helpers';
+import { buildChartSeries, chartOptions } from './helpers';
 import './styles.css';
 
-const initialData = [
-  {
-    x: '2020-01-01',
-    y: 45
-  },
-  {
-    x: '2020-01-02',
-    y: 55
-  },
-  {
-    x: '2020-01-03',
-    y: 39
-  },
-  {
-    x: '2020-01-04',
-    y: 25
-  },
-  {
-    x: '2020-01-05',
-    y: 35
-  },
-  {
-    x: '2020-01-06',
-    y: 20
-  },
-  {
-    x: '2020-01-07',
-    y: 50
-  },
-  {
-    x: '2020-01-08',
-    y: 30
-  }
-];
-
-function SalesByDate() {
-  const [salesByDate, setSalesByDate] = useState();
+function SalesByDateComponent() {
+  const [chartSeries, setChartSerires] = useState<ChartSeriesData[]>([]);
 
   useEffect(() => {
     makeRequest
-      .get('/sales/by-date?minDate=2017-01-01&maxDate=2017-01-31&gender=FEMALE')
+      .get<SalesByDate[]>('/sales/by-date?minDate=2017-01-01&maxDate=2017-01-31&gender=FEMALE')
       .then((response) => {
-        console.log(response.data);
+        const newChartSeries = buildChartSeries(response.data);
+        setChartSerires(newChartSeries);
       });
   }, []);
 
@@ -67,7 +34,7 @@ function SalesByDate() {
         <div className="sales-by-date-chart">
           <ReactApexChart
             options={chartOptions}
-            series={[{ name: 'Vendas', data: initialData }]}
+            series={[{ name: 'Vendas', data: chartSeries }]}
             type="bar"
             height={240}
             width="100%"
@@ -78,4 +45,4 @@ function SalesByDate() {
   );
 }
 
-export default SalesByDate;
+export default SalesByDateComponent;
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -2,3 +2,8 @@ export type SalesByDate = {
   date: string;
   sum: number;
 };
+
+export type ChartSeriesData = {
+  x: string;
+  y: number;
+};

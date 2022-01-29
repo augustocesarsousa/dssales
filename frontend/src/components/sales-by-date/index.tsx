9e4256f9d035f0d95df678bef8cc90e93c0fcaae
Augusto Cesar <acs-03@hotmail.com>
2022-01-28 22:20:40 -0300

    Backend integration SalesByDate - part 1

--- frontend/src/components/sales-by-date/index.tsx
+++ frontend/src/components/sales-by-date/index.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import ReactApexChart from 'react-apexcharts';
+import { makeRequest } from '../../utils/request';
 import { chartOptions } from './helpers';
 import './styles.css';
 
@@ -38,6 +40,16 @@ const initialData = [
 ];
 
 function SalesByDate() {
+  const [salesByDate, setSalesByDate] = useState();
+
+  useEffect(() => {
+    makeRequest
+      .get('/sales/by-date?minDate=2017-01-01&maxDate=2017-01-31&gender=FEMALE')
+      .then((response) => {
+        console.log(response.data);
+      });
+  }, []);
+
   return (
     <div className="sales-by-date-container base-card">
       <div>
---
+++ frontend/src/types.ts
@@ -0,0 +1,4 @@
+export type SalesByDate = {
+  date: string;
+  sum: number;
+};

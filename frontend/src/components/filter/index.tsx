ada4a3628a4ba2454e1c866ae0dfba2b19689b08
Augusto Cesar <acs-03@hotmail.com>
2022-01-29 20:05:20 -0300

    Filter integration with aplication

--- frontend/src/components/filter/index.tsx
+++ frontend/src/components/filter/index.tsx
@@ -1,14 +1,32 @@
 import flatpickrlib from 'flatpickr';
 import { Portuguese } from 'flatpickr/dist/l10n/pt';
 import 'flatpickr/dist/themes/material_green.css';
+import React, { useState } from 'react';
 import FlatPicker from 'react-flatpickr';
+import { FilterData, Gender } from '../../types';
 import './styles.css';
 
 flatpickrlib.localize(Portuguese);
 
-function Filter() {
+type Props = {
+  onFilterChange: (filter: FilterData) => void;
+};
+
+function Filter({ onFilterChange }: Props) {
+  const [dates, setDates] = useState<Date[]>([]);
+  const [gender, setGender] = useState<Gender>();
+
   const onChangeDate = (dates: Date[]) => {
-    console.log(dates);
+    if (dates.length === 2) {
+      setDates(dates);
+      onFilterChange({ dates, gender });
+    }
+  };
+
+  const onChangeGender = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedGender = event.target.value as Gender;
+    setGender(selectedGender);
+    onFilterChange({ dates, gender: selectedGender });
   };
 
   return (
@@ -23,7 +41,7 @@ function Filter() {
         onChange={onChangeDate}
         placeholder="Selecione um período"
       />
-      <select className="filter-input">
+      <select className="filter-input" value={gender} onChange={onChangeGender}>
         <option value="">Selecione um gênero</option>
         <option value="MALE">Masculino</option>
         <option value="FEMALE">Feminino</option>

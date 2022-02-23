7190bd76e7db8cf90c198594b3817685159ad6c5
Augusto Cesar <acs-03@hotmail.com>
2022-02-22 22:36:09 -0300

    Added clear button and implemented reack-hook-form in filter

--- frontend/src/components/filter/index.tsx
+++ frontend/src/components/filter/index.tsx
@@ -3,6 +3,7 @@ import { Portuguese } from 'flatpickr/dist/l10n/pt';
 import 'flatpickr/dist/themes/dark.css';
 import React, { useState } from 'react';
 import FlatPicker from 'react-flatpickr';
+import { Controller, useForm } from 'react-hook-form';
 import { FilterData, Gender } from '../../types';
 import './styles.css';
 
@@ -13,39 +14,79 @@ type Props = {
 };
 
 function Filter({ onFilterChange }: Props) {
-  const [dates, setDates] = useState<Date[]>([]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [gender, setGender] = useState<Gender>();
+
+  const { register, handleSubmit, setValue, getValues, control } = useForm<FilterData>();
+
+  const onSubmit = (formData: FilterData) => {
+    onFilterChange(formData);
+  };
 
   const onChangeDate = (dates: Date[]) => {
     if (dates.length === 0 || dates.length === 2) {
-      setDates(dates);
-      onFilterChange({ dates, gender });
+      setValue('dates', dates);
+      const obj: FilterData = {
+        dates: getValues('dates'),
+        gender: getValues('gender')
+      };
+      onFilterChange(obj);
     }
   };
 
   const onChangeGender = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedGender = event.target.value as Gender;
-    setGender(selectedGender);
-    onFilterChange({ dates, gender: selectedGender });
+    setValue('gender', event.target.value as Gender);
+    const obj: FilterData = {
+      dates: getValues('dates'),
+      gender: getValues('gender')
+    };
+    onFilterChange(obj);
+  };
+
+  const handleFormClear = () => {
+    setValue('dates', []);
+    setValue('gender', undefined);
+    const obj: FilterData = {
+      dates: getValues('dates'),
+      gender: getValues('gender')
+    };
+    onFilterChange(obj);
   };
 
   return (
     <div className="filter-container base-card">
-      <FlatPicker
-        options={{
-          mode: 'range',
-          dateFormat: 'd/m/Y'
-        }}
-        className="filter-input"
-        onChange={onChangeDate}
-        placeholder="Selecione um período"
+      <form onSubmit={handleSubmit(onSubmit)}></form>
+      <Controller
+        name="dates"
+        control={control}
+        render={({ field }) => (
+          <FlatPicker
+            {...field}
+            options={{
+              mode: 'range',
+              dateFormat: 'd/m/Y'
+            }}
+            className="filter-input"
+            onChange={onChangeDate}
+            placeholder="Selecione um período"
+          />
+        )}
       />
-      <select className="filter-input" value={gender} onChange={onChangeGender}>
+
+      <select
+        {...register('gender')}
+        className="filter-input"
+        value={gender}
+        onChange={onChangeGender}
+      >
         <option value="">Selecione um gênero</option>
         <option value="MALE">Masculino</option>
         <option value="FEMALE">Feminino</option>
         <option value="OTHER">Outro</option>
       </select>
+      <button className="filter-button" onClick={handleFormClear}>
+        Limpar filtro
+      </button>
     </div>
   );
 }
